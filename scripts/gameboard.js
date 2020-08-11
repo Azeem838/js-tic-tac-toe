dbfa7bb5abb2750ded4fcd594ee83e18f3a75b37
Azeem <azeemahmed101@gmail.com>
2020-08-11 14:37:10 +0200

    add winner game logic

--- scripts/gameboard.js
+++ scripts/gameboard.js
@@ -1,51 +1,81 @@
 // import {  } from "module";
 
-
 const Player = (name) => {
   return { name };
 };
 
 const gameBoard = (() => {
-  const gameboard = ['','','','','','','','',''];
+  let gameboard = ["", "", "", "", "", "", "", "", ""];
+  const winArr = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
 
-  const player1 = Player('eric');
-  const player2 = Player('azeem');
+  const player1 = Player("eric");
+  const player2 = Player("azeem");
 
   let currentPlayer = player1;
 
   const switchPlayer = () => {
     if (currentPlayer === player1) {
-      return currentPlayer = player2;
-    }else {
-      return currentPlayer = player1
+      return (currentPlayer = player2);
+    } else {
+      return (currentPlayer = player1);
     }
   };
 
   const render = () => {
-    for(let i = 0; i < 9; i += 1) {
+    for (let i = 0; i < 9; i += 1) {
       const box = document.querySelector(`.box[data-set="${i}"]`);
       box.innerHTML = gameboard[i];
-    };
-  }
+    }
+  };
 
   const boxClicked = () => {
-    const boxes = document.querySelectorAll('.box');
-    boxes.forEach(box => {
-      box.addEventListener('click', markBox);
+    const boxes = document.querySelectorAll(".box");
+    boxes.forEach((box) => {
+      box.addEventListener("click", markBox);
     });
-  }
+  };
+
+  const winner = () => {
+    for (let i = 0; i < 8; i++) {
+      let first = gameboard[winArr[i][0]];
+      let second = gameboard[winArr[i][1]];
+      let third = gameboard[winArr[i][2]];
+      if (
+        first !== "" &&
+        second !== "" &&
+        third !== "" &&
+        first === second &&
+        second === third
+      ) {
+        alert("Congrats " + currentPlayer.name + " you have won!");
+      }
+    }
+  };
 
   const markBox = (e) => {
-    if (currentPlayer === player1 && e.target.innerHTML === '') {
+    if (currentPlayer === player1 && e.target.innerHTML === "") {
       e.target.innerHTML = "O";
+      gameboard[e.target.dataset.set] = "O";
+      winner();
       switchPlayer();
-    } else if(currentPlayer === player2 && e.target.innerHTML === '') {
+    } else if (currentPlayer === player2 && e.target.innerHTML === "") {
       e.target.innerHTML = "X";
+      gameboard[e.target.dataset.set] = "X";
+      winner();
       switchPlayer();
     }
-  }
+  };
 
-  return { gameboard, render, boxClicked, switchPlayer };
+  return { gameboard, render, boxClicked, markBox, winner, winArr };
 })();
 
 gameBoard.render();
