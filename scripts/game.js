d0b804c929e57c573e92d74734ed756eae326e30
Azeem <azeemahmed101@gmail.com>
2020-08-13 10:11:11 +0200

    fix reset function

--- scripts/game.js
+++ scripts/game.js
@@ -2,8 +2,9 @@ import { displayController } from "./dom.js";
 import { Game } from "./app.js";
 import { Player } from "./player.js";
 
+let gameboard = ["", "", "", "", "", "", "", "", ""];
+
 const start = () => {
-  let gameboard = ["", "", "", "", "", "", "", "", ""];
   let firstPlayerName = document.querySelector("#player1").value;
   let secondPlayerName = document.querySelector("#player2").value;
   const player1 = Player(firstPlayerName);
@@ -21,7 +22,11 @@ const start = () => {
 };
 
 const reset = () => {
-  return start();
+  gameboard = ["", "", "", "", "", "", "", "", ""];
+  displayController.render(gameboard);
+
+  document.querySelector("#player1").value = "";
+  document.querySelector("#player2").value = "";
 };
 
 displayController.btnListners(start, reset);
